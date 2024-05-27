a2a155320a6bfff02b76251363a1632a0fceeafa
minki-j <qmsoqm2@gmail.com>
2024-05-27 08:26:19 -0400

    start chat application using Vercel's open source template

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,16 +1,8 @@
-import { authMiddleware } from "@clerk/nextjs";
+import NextAuth from 'next-auth'
+import { authConfig } from './auth.config'
 
-export default authMiddleware({
-  // Allow signed out users to access the specified routes:
-  publicRoutes: ['/'],
-});
+export default NextAuth(authConfig).auth
 
 export const config = {
-  matcher: [
-    // Exclude files with a "." followed by an extension, which are typically static files.
-    // Exclude files in the _next directory, which are Next.js internals.
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    // Re-include any files in the api or trpc folders that might have an extension
-    "/(api|trpc)(.*)",
-  ],
-};
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)']
+}
