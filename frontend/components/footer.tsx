8523aeb942d3b3e82e05baa54da4560b8a218d58
minki-j <qmsoqm2@gmail.com>
2024-05-31 08:19:29 -0400

    add footer message

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -12,12 +12,7 @@ export function FooterText({ className, ...props }: React.ComponentProps<'p'>) {
       )}
       {...props}
     >
-      Open source AI chatbot built with{' '}
-      <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-      <ExternalLink href="https://github.com/vercel/ai">
-        Vercel AI SDK
-      </ExternalLink>
-      .
+      Built with 🔥 by <ExternalLink href="https://github.com/minki-j"> Minki Jung</ExternalLink>
     </p>
   )
 }
